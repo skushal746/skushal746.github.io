bfb2b35357ee6d04b19e23234095b9f19699d1de
Kushal Sharma <skushal746@gmail.com>
2022-02-06 23:48:01 -0500

    Committing scripts

--- js/scripts.js
+++ js/scripts.js
@@ -97,4 +97,19 @@
         });
     });
 
+    var message = "";
+
+    $("#sendMessage").on("click", function() {
+        message = $("#contactform").serialize();
+        $.ajax({
+            url: "//formspree.io/dynamicrealities@gmail.com", 
+            method: "POST",
+            data: {message: message},
+            dataType: "json"
+        });
+        alert('Thanks for the email, we\'ll be in touch promptly.');
+        return false;
+    });
+
+
 })(jQuery);
