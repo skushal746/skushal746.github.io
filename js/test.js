c83a2ee4e155a6baf0882b8d2066b957fe1fcf87
Kushal Sharma <skushal746@gmail.com>
2022-02-07 17:42:34 -0500

    Trying to make contact me as a ajax call

--- js/test.js
+++ js/test.js
@@ -1,13 +1,30 @@
-var message = "";
-$("#sendMessage").on("click", function() {
-    console.log("Entered in javascript code");
-    message = $("#contactform").serialize();
-    $.ajax({
-        url: "//formspree.io/dynamicrealities@gmail.com", 
-        method: "POST",
-        data: {message: message},
-        dataType: "json"
+(function($) {
+    var message = "";
+
+    $("#sendMessage").on("click", function() {
+        console.log("Entered in javascript code 1");
+        message = $("#contactform").serialize();
+        $.ajax({
+            url: "//formspree.io/dynamicrealities@gmail.com", 
+            method: "POST",
+            data: {message: message},
+            dataType: "json"
+        });
+        alert('Thanks for the email, we\'ll be in touch promptly.');
+        return false;
     });
-    alert('Thanks for the email, we\'ll be in touch promptly.');
-    return false;
-});
+
+    $("#sendMessageToKushal").on("click", function() {
+        console.log("Entered in javascript code 2");
+        message = $("#contactform").serialize();
+        $.ajax({
+            url: "//formspree.io/dynamicrealities@gmail.com", 
+            method: "POST",
+            data: {message: message},
+            dataType: "json"
+        });
+        alert('Thanks for the email, we\'ll be in touch promptly.');
+        return false;
+    });
+
+})(jQuery);
